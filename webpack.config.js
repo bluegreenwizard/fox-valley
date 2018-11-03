6a8d7b256f29608688207aa4c715de2726602825
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2018-11-02 21:06:47 -0500

    Installed dependencies

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist/')
+	},
+	devtool: "source-map",
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true,
+			inject: 'body',
+			template: 'src/index.pug'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test:/\.pug$/,
+				use: 'pug-loader'
+			},
+			{
+				test:/\.s?[a|c]ss$/,
+				use: [
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' }
+				]
+			},
+			{
+				test:/\.(jpeg|jpg|png|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: './img'
+						}
+					} 
+				]
+			}
+		]
+	}
+};
