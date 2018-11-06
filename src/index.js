c6f3f0dd92b247669ae5e3ce76cc02434a201aae
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2018-11-05 23:14:08 -0600

    Blockquote redesign

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import 'normalize.css'
 import './style/style.sass';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.quote').classList.add('quote-loaded');
+});
